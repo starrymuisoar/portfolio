bab6a78b663440373bf2a327dd7a2632986e380d
starrymuisoar <80806849+starrymuisoar@users.noreply.github.com>
2021-04-25 05:05:33 +0900

    Add files via upload

--- js/layout.js
+++ js/layout.js
@@ -1,3 +1,19 @@
-$(function(){
-	
+jQuery(document).ready(function(){
+	$(".navlist > li").mouseover(function(){
+		if($(this).find(".submenu").length > 0){
+			$(this).find(".submenu").stop().slideDown(500);
+		} 
+	});
+	$(".navlist > li").mouseout(function(){
+		$(this).find(".submenu").stop().slideUp(500);
+	});
+	$('.trigger').on('click', function(){
+		$('.modal_contact').toggleClass('open');
+		return false;
+	})
+	$('#copy_email').click(function(){
+		$('#textbox_email').select()
+		document.execCommand("copy")
+		alert('E-mail 주소가 복사 되었습니다.')
+	});
 });
