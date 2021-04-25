3125544fb2bd0847a5a55b36a8d5953dfad11aa9
starrymuisoar <80806849+starrymuisoar@users.noreply.github.com>
2021-04-26 02:07:11 +0900

    Add files via upload

--- js/layout.js
+++ js/layout.js
@@ -1,19 +1,81 @@
-jQuery(document).ready(function(){
-	$(".navlist > li").mouseover(function(){
-		if($(this).find(".submenu").length > 0){
-			$(this).find(".submenu").stop().slideDown(500);
-		} 
+$(function(){
+	$("nav .mbtn").on("click", function(){
+		if($("nav").hasClass("on")){
+			$("nav").removeClass("on");
+		} else{
+			$("nav").addClass("on");
+		}
+	}); 
+	$(".navlist > li").find(".dp2").parent().addClass("act");
+	$(".navlist > li > a").on("click", function(){
+		var _this = $(this);
+		var _thispt = _this.parent();
+		var _target = _thispt.find(".dp2");
+		if(_thispt.hasClass("on")){
+			_thispt.removeClass("on");
+			_target.slideUp();
+		} else{$(".navlist > li").removeClass("on");
+			_thispt.addClass("on");
+			$(".navlist .dp2").slideUp();
+			_target.slideDown();
+			}
 	});
-	$(".navlist > li").mouseout(function(){
-		$(this).find(".submenu").stop().slideUp(500);
-	});
-	$('.trigger').on('click', function(){
-		$('.modal_contact').toggleClass('open');
+	$('.tabmenu > li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+		
+		$('.tabmenu > li').removeClass('active');
+		$('.tab-content').removeClass('active');
+		
+		$(this).addClass('active');
+		$("#"+tab_id).addClass('active');
+		
 		return false;
-	})
-	$('#copy_email').click(function(){
-		$('#textbox_email').select()
-		document.execCommand("copy")
-		alert('E-mail 주소가 복사 되었습니다.')
 	});
+	
+	var gall = setInterval(galleryFun, 5000);
+	var inter = true;
+	var idx = 2;
+	function galleryFun(){
+		$(".tab-content > ul").animate({
+			"left":-300*idx+"px"
+		},300);
+		$(".g_item > ul > li").eq(idx-1).addClass("on").silbling().removeClass("on");
+		idx++;
+		if(idx> $(".tab-content > ul > li").length-3){
+			$(".tab-content > ul").animate({
+				"left":0
+			},0);
+			idx=0;
+			}
+		}
+	$(".tab-content, .g_item").hover(function(){
+		if(inter==true){
+			clearInterval(gall);
+			inter=false;
+		}
+	},function(){
+		if(inter==false){
+			gall = setInterval(galleryFun,5000);
+			inter=true;
+		}
+	});
+	
+	$(".g_item > ul > li").on('click',function(){
+		$(this).addClass("on").siblings().removeClass("on");
+		idx = $(this).index()+1;
+		$("tab-content > ul").animate({
+			"left":-300*idx+"px"
+		},1000);
+	});
+
 });
+
+
+
+
+
+
+
+
+
+
